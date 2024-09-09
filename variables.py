a108b2c0dd96a3289e6c2769ec1c5ff4b6858d8c
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-09-09 09:29:36 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,13 @@
 fname = "Aliyah" # string variable
 age= 16 # number variable
 fact = True # Boolean Variable
+
+print(13) #This prints a number
+print("We are the best") #This prints a string
+#print data from a variable 
+print(fname)
+print(age)
+print(fact)
+
+question = input("what day is it?")
+print(fname,"Happy" + question+ "!")
