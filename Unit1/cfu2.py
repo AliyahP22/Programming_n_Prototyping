d2901b5add9c51267b2a7e25e16e2c4835a04e53
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-09-16 09:26:47 -0400

    Create cfu2.py

---
+++ Unit1/cfu2.py
@@ -0,0 +1,6 @@
+fname = input("What is your first name?")
+lname = input("What is your last name?")
+age =int(input("What is your age?"))
+doing = input("How long have you been learning coding?")
+
+print(" Hello " + fname  +  lname   +   str( age)  +  doing)
