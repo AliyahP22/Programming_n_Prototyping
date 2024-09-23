c7f134a24924b760bd1d8271b00085205db7a08d
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-09-23 09:27:00 -0400

    Create practiceunit2.py

---
+++ practiceunit2.py
@@ -0,0 +1,5 @@
+import math
+r = float(input("What is the radius of your cylinder?"))
+h = int(input("What is the height of your cylinder?"))
+v = math.pi * r**2 * h
+print("The volume of your cylinder is " + "v=" ,v)
