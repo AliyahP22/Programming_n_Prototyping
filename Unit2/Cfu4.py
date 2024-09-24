7c155cde88e334b864be3278bc589ca210e7da0c
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-09-24 08:58:26 -0400

    Update Cfu4.py

--- Unit2/Cfu4.py
+++ Unit2/Cfu4.py
@@ -1,1 +1,14 @@
+#09-24-2024 Aliyah Pargan Periods 1-2. This CFU demonstrates the skills of using import math and how you can solve
+#advanced math equations using python.
+fname = input("What is your name ")
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+import math
+root1 = (-b + (b ** 2 - 4 * a * c) **0.5) /(2 * a)
+root2 =  (-b - (b ** 2 - 4 * a * c) **0.5) /(2 * a)
+
+print(fname + " the roots of your equation are " + str(root1) , str(root2))
 
