9dacf2228ddeb1b9710be71496ee911657890c44
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-09-29 00:46:35 -0400

    Create groupchallenge.py

---
+++ Unit1/groupchallenge.py
@@ -0,0 +1,7 @@
+#Challenge 1
+print("Perimeter of a rectangle")
+length = int(input("Enter a number: "))
+width = int(input("Enter a number: "))
+perimeter = (2 * length) + (2 * width)
+print("Perimeter of a " + str(length) + " by " +  str(width) + " rectangle is " + str(perimeter))
+
