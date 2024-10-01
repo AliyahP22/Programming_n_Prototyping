b000a83131e4335d3c298353933ef6ab1e25c1ea
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-01 09:33:29 -0400

    Update Cfu5challenge.py

--- Unit2/Cfu5challenge.py
+++ Unit2/Cfu5challenge.py
@@ -16,3 +16,34 @@ nickelstopennies = nickels * 5
 quarterstopennies = quarters * 25
 looniestopennies = loonies * 100
 tooniestopennies = toonies * 200
+#10-1-2024 Aliyah Pargan Periods 1-2
+
+pennies = int(input("Enter an amount of pennies: "))
+dimes = int(input("Enter an amount of dimes: "))
+nickels = int(input("Enter an amount of nickels: "))
+quarter = int(input("Enter an amount of quarters: "))
+loonies = int(input("Enter an amount of loonies: "))
+toonies = int(input("Enter an amount of toonies: "))
+
+dimestopennies = dimes * 10
+nickelstopennies = nickels * 5
+quarterstopennies = quarter * 25
+looniestopennies = loonies * 100
+tooniestopennies = toonies * 200
+totalpennies = dimestopennies + pennies + nickelstopennies + quarterstopennies+ looniestopennies + tooniestopennies
+total = totalpennies / 100
+
+
+print("The total amount of money you have is $" + str(total))
+print("Total value of coins: $" + "{:.2f}" .format(total_value))
+ totalDollar = int(total_value // 1)
+ print(totalDollar)
+change = total_value - totalDollar
+change_quarter = (change) % 4
+inCents = int(change*100)
+inQuart = int(change*4)
+inDimes = int)change*10)-inQuart
+inNickels = int(change*20)-(inDimes*2)
+inCents = int(change*100)-(inNickels*10)
+print(f"Your total is: ${totalDollars},{inQuarts}quarters, {inDimes}, dimes, {inNickels}nickles, {inCents}cents)
+
