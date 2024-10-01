189cfffe665d6d0d094cb012958f126f8ffe6e55
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-01 08:28:17 -0400

    Update Cfu5challenge.py

--- Unit2/Cfu5challenge.py
+++ Unit2/Cfu5challenge.py
@@ -4,3 +4,15 @@
 # pennies = 1 cent nickles = 5 cents dimes = 10cents quarters = 25c loonies = $1 or 100 cents toonies = $2 or 200 cents 
 #ask kid how much of each coin they have and calculate the total and give answer in decimals and in a combination
 
+pennies = int(input("Enter an amount of pennies: "))
+dimes = int(input("Enter an amount of dimes: "))
+nickels = int(input("Enter an amount of nickels: "))
+quarter = int(input("Enter an amount of quarters: "))
+loonies = int(input("Enter an amount of loonies: "))
+toonies = int(input("Enter an amount of toonies: "))
+
+dimestopennies = dimes * 10
+nickelstopennies = nickels * 5
+quarterstopennies = quarters * 25
+looniestopennies = loonies * 100
+tooniestopennies = toonies * 200
