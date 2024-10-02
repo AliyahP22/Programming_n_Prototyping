fc383b52601bbd393d23de0b966a0313719cb8b7
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-01 21:17:45 -0400

    Update Cfu5challenge.py

--- Unit2/Cfu5challenge.py
+++ Unit2/Cfu5challenge.py
@@ -4,20 +4,7 @@
 # pennies = 1 cent nickles = 5 cents dimes = 10cents quarters = 25c loonies = $1 or 100 cents toonies = $2 or 200 cents 
 #ask kid how much of each coin they have and calculate the total and give answer in decimals and in a combination
 
-pennies = int(input("Enter an amount of pennies: "))
-dimes = int(input("Enter an amount of dimes: "))
-nickels = int(input("Enter an amount of nickels: "))
-quarter = int(input("Enter an amount of quarters: "))
-loonies = int(input("Enter an amount of loonies: "))
-toonies = int(input("Enter an amount of toonies: "))
-
-dimestopennies = dimes * 10
-nickelstopennies = nickels * 5
-quarterstopennies = quarters * 25
-looniestopennies = loonies * 100
-tooniestopennies = toonies * 200
 #10-1-2024 Aliyah Pargan Periods 1-2
-
 pennies = int(input("Enter an amount of pennies: "))
 dimes = int(input("Enter an amount of dimes: "))
 nickels = int(input("Enter an amount of nickels: "))
@@ -30,20 +17,24 @@ nickelstopennies = nickels * 5
 quarterstopennies = quarter * 25
 looniestopennies = loonies * 100
 tooniestopennies = toonies * 200
-totalpennies = dimestopennies + pennies + nickelstopennies + quarterstopennies+ looniestopennies + tooniestopennies
-total = totalpennies / 100
+total_pennies = dimestopennies + pennies + nickelstopennies + quarterstopennies+ looniestopennies + tooniestopennies
+dollars = total_pennies // 100  
+cents = total_pennies % 100  
 
 
-print("The total amount of money you have is $" + str(total))
-print("Total value of coins: $" + "{:.2f}" .format(total_value))
- totalDollar = int(total_value // 1)
- print(totalDollar)
-change = total_value - totalDollar
-change_quarter = (change) % 4
-inCents = int(change*100)
-inQuart = int(change*4)
-inDimes = int)change*10)-inQuart
-inNickels = int(change*20)-(inDimes*2)
-inCents = int(change*100)-(inNickels*10)
-print(f"Your total is: ${totalDollars},{inQuarts}quarters, {inDimes}, dimes, {inNickels}nickles, {inCents}cents)
+print(f"You have a total of ${dollars}.{cents:02d}!")
+
+
+remaining_cents = cents
+num_quarters = int(remaining_cents // 25)
+remaining_cents = int(remaining_cents % 25)
+num_dimes = int(remaining_cents // 10)
+remaining_cents = int(remaining_cents % 10)
+num_nickels = int(remaining_cents // 5)
+remaining_cents = int(remaining_cents % 5)
+num_pennies = int(remaining_cents)
+
+print(f"That's {dollars} dollars, {num_quarters} quarters, {num_dimes} dimes, {num_nickels} nickels, and {num_pennies} pennies.")
+
+
 
