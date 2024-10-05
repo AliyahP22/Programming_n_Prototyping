89f6f15119643fd5842a1e2efe6446fd20bf670e
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-05 19:31:42 -0400

    Update CFU6.py

--- Unit2/CFU6.py
+++ Unit2/CFU6.py
@@ -1,1 +1,19 @@
+print("Hi! Welcome to the Random Number Calculator!")
+print("I will generate a random number between 1 and 100")
+
+import random
+num_random = random.randint(1,100)
+user_number = int(input("Please enter a number: "))
+
+print(" ")
+print("Random number generated: " + str(num_random))
+print("Please enter a number: " + str(user_number))
+
+import math
+print(" ")
+print("Results: ")
+print("Your Number: " + str(user_number))
+print("
+
+
 
