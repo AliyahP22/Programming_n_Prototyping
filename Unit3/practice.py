23b696da4cb05e815d52106d494faa7896fc1994
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-08 09:12:08 -0400

    Update practice.py

--- Unit3/practice.py
+++ Unit3/practice.py
@@ -6,3 +6,11 @@ def my_first_func():
 my_first_func()
 my_first_func()
 my_first_func()
+
+
+def mult_2_nums(num1, num2):
+    print(num1 * num2)
+    
+    
+mult_2_nums(5,4)
+mult_2_nums(10, -3.5)
