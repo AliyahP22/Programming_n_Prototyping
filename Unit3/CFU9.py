8b3d9b7c81c2cfa1ffc7358164af4444a63db63a
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-18 11:31:40 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -4,19 +4,29 @@
 #what the number is. 
 
 
-num_rolls = int(input("How many rolls do you want to play?"))
-print("Amount of rolls: ", num_rolls)
-
-score = 0
 import random
-for roll in range(1, num_rolls + 1 ): #loop used to help code work (from w3schools)
-    rolls = random.randint(1,6)
-    user_guess = int(input(f"Roll {roll}: Guess a number between 1 and 6: "))
-    if user_guess == rolls:
-        print("Congrats! You guessed right! 6+ points")
-        score += 6 #adds points
+print("Let's roll the dice!")
+num_rolls = int(input("How many times do we want to roll the dice?"))
+
+def guess(num_random,total):
+    
+    guess = int(input("Guess the roll: "))
+    if guess == num_random:
+        total = total +6
     else:
-        print("Sorry, that wasn't the number... -1 point")
-        score -= 1 #subtracts points
+        total = total -1
         
-print("Your final score is: " + str(score))
+        
+def rolls(num_rolls, total =0):
+    num_random = random.randint(1,6)
+    if num_rolls ==0:
+        return total
+    else:
+        num_rolls = num_rolls -1
+        guess(num_random,total)
+        print(f"you roll a {num_random}!")
+        return rolls(num_rolls,total)
+    
+total =rolls(num_rolls)
+print(f"total is: {total}")
+
