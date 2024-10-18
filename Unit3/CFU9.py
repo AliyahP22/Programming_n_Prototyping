c97beaa88440df4db9a2b41fff6e03978f3318e1
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-17 20:13:35 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -5,14 +5,18 @@
 
 
 num_rolls = int(input("How many rolls do you want to play?"))
+print("Amount of rolls: ", num_rolls)
 
+score = 0
 import random
-rolls = random.randint(1,6)
-user_guess = int(input("What number do you guess?"))
-
-print(rolls)
-
-if user_guess == rolls:
-    print("Congrats! You guessed right! 6+ points")
-else: 
-    print("Sorry, that wasn't the number... -1 point")
+for roll in range(1, num_rolls + 1 ): #loop used to help code work (from w3schools)
+    rolls = random.randint(1,6)
+    user_guess = int(input(f"Roll {roll}: Guess a number between 1 and 6: "))
+    if user_guess == rolls:
+        print("Congrats! You guessed right! 6+ points")
+        score += 6 #adds points
+    else:
+        print("Sorry, that wasn't the number... -1 point")
+        score -= 1 #subtracts points
+        
+print("Your final score is: " + str(score))
