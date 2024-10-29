cff39d5644ca019ca365a59ba05d203e0166e4ef
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-29 09:27:16 -0400

    Update CFU12.py

--- Unit4/CFU12.py
+++ Unit4/CFU12.py
@@ -2,5 +2,14 @@
 #CFU 12, which has a set password, allows users to keep guessing
 #until they get the right password. This program uses while loops
 
-
+#Version 1
 password = "simonsync"
+password = "simonsync"
+user_guess = " "
+
+while password != user_guess:
+    print("Wrong, please try again")
+    user_guess = input("What is the password?")
+
+    
+print("Correct, you may enter!!")
