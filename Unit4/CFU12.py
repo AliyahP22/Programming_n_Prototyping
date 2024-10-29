d2e81610fc5e499d78aeb16825864a6632325af0
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-29 09:24:26 -0400

    Update CFU12.py

--- Unit4/CFU12.py
+++ Unit4/CFU12.py
@@ -1,1 +1,6 @@
+#Periods 1-2 Aliyah Pargan 10-29-2024
+#CFU 12, which has a set password, allows users to keep guessing
+#until they get the right password. This program uses while loops
 
+
+password = "simonsync"
