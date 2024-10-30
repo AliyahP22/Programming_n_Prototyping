fa0bb3b3bcef5f38fc1c437be7f79eaa7463bb31
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-10-29 21:36:17 -0400

    Update CFU12.py

--- Unit4/CFU12.py
+++ Unit4/CFU12.py
@@ -2,18 +2,38 @@
 #CFU 12, which has a set password, allows users to keep guessing
 #until they get the right password. This program uses while loops
 
-password = "simonsync"
-user_guess = " "
-num_tries = 3
 
-while password != user_guess:
-    while num_tries < 3:
+def version_1():
+    password = "simonsync"
+    user_guess = " "
+    while password != user_guess:
         print("Wrong, please try again")
         user_guess = input("What is the password?")
-        if num_tries == 0:
-            print("Out of tries, Sorry!")
-
     
-print("Correct, you may enter!!")
+    print("Correct, you may enter!!")
 
+
+def version_2():
+    password = "simonsync"
+    num_tries = 0
+    while num_tries < 3:
+        user_guess = input("What is the password?")
+        if user_guess != password:
+             print("Wrong, please try again")
+             num_tries+=1
+         
+        else:
+            print("Correct, you may enter!!")
+            num_tries+=1
+            return user_guess
+        print("Max Attempts: 3")
+        print(f"Your Attempts: {num_tries}")
+        if num_tries == 3:
+            print("Out of attempts, try again!")
+        else:
+            print(" ")
+            
+
+version_1()            
+version_2()
 
