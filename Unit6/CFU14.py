5b2db6f66f8e92db63774b41530192bceaeabf33
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-11-26 18:46:54 -0500

    Update CFU14.py

--- Unit6/CFU14.py
+++ Unit6/CFU14.py
@@ -1,1 +1,5 @@
-
+#Aliyah Pargan
+#Period 2
+#11-26-2024
+#In CFU 14, using the canvas we have created, we are going to create a smiley face using the canvas draw_line method and using this method,
+#I would adjust the line width and height.
