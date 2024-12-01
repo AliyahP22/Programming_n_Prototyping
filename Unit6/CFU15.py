08dac422a1f320cb3f3be4de1d8c01bef9dd68b7
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-11-30 21:18:14 -0500

    Update CFU15.py

--- Unit6/CFU15.py
+++ Unit6/CFU15.py
@@ -1,1 +1,19 @@
+import simplegui
 
+
+def draw_handler(canvas):
+    #left eye
+   canvas.draw_polygon([(70,100),(75,102),(80,105)], 5, "black")
+   
+
+
+
+
+#Creates frame
+frame = simplegui.create_frame("CFU15 Happy Shapes",200,200)
+frame.set_canvas_background("pink")
+frame.set_draw_handler(draw_handler)
+
+
+#start frame
+frame.start()
