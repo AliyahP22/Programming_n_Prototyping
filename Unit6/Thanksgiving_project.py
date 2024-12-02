40392bcd6d2c1dc2d56a0b2b6229fda1d974d858
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-12-02 09:07:42 -0500

    Update Thanksgiving_project.py

--- Unit6/Thanksgiving_project.py
+++ Unit6/Thanksgiving_project.py
@@ -10,7 +10,7 @@ import simplegui
 message = "Happy Thanksgiving!"
 #create frame
 frame = simplegui.create_frame("Thanksgiving Drawing", 600,600)
-frame.set_canvas_background("black")
+frame.set_canvas_background("chocolate")
 
 
 #my function
@@ -21,13 +21,14 @@ def click_button():
 # Draw handler function
 def draw_handler(canvas):
     #Adding a message to frame
-    canvas.draw_text(message, [70,100],40,"Maroon")
+    canvas.draw_text(message, [120,100],40,"Maroon")
     # Example: Drawing a circle
     canvas.draw_circle((300, 200), 60, 5, "SaddleBrown", "SaddleBrown")
     canvas.draw_circle((313, 200), 5, 5, "black", "black")
     canvas.draw_circle((280, 200), 5, 5, "black", "black")
     canvas.draw_polygon([(320, 210), (285, 210), (312, 227)], 5, "Red", "Yellow")
-    canvas.draw_circle((300, 330), 80, 5, "SaddleBrown", "SaddleBrown")
+    canvas.draw_circle((220, 320), 50, 5, "Gold", "FireBrick")
+    canvas.draw_circle((300, 330), 90, 5, "SaddleBrown", "SaddleBrown")
     
   
    
@@ -38,7 +39,7 @@ def draw_handler(canvas):
     # Assign draw handler to the frame
 frame.set_draw_handler(draw_handler)
 #add button code
-frame.add_button("Click here!", click_button)#calls function
+frame.add_button("Click here!", click_button) #calls function
 
 
 
