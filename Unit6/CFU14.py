36ba57548da0f3c35e4d06b18f9fb10714fa0602
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2024-12-02 09:30:36 -0500

    Update CFU14.py

--- Unit6/CFU14.py
+++ Unit6/CFU14.py
@@ -1,33 +1,54 @@
 #Aliyah Pargan
-#Period 2
-#11-26-2024
-#In CFU 14, using the canvas we have created, we are going to create a smiley face using the canvas draw_line method and using this method,
-#I would adjust the line width and height.
-import simplegui 
+#Periods 1-2
+#12-2-2024
+#The Thanksgiving Drawing project is a drawing created by all the functions learned so far, such as drawing polygons
+#and drawing circles. I would also be using my own functions to make it fun and interactive! The main goal is to create
+#a cute and festive drawing using python
 
+
+import simplegui
+#initial message displayed
+message = "Happy Thanksgiving!"
+#create frame
+frame = simplegui.create_frame("Thanksgiving Drawing", 600,600)
+frame.set_canvas_background("chocolate")
+
+
+#my function
+def click_button():
+    global message
+    message = "Enjoy the Holiday Season!"#displayed after button is clicked
+    
+# Draw handler function
 def draw_handler(canvas):
-    # Left Eye
-    canvas.draw_line([70,100],[75,102], 5,"blue")
+    #Adding a message to frame
+    canvas.draw_text(message, [120,100],40,"Maroon")
+    # creating the turkey
+    canvas.draw_circle((300, 200), 60, 5, "SaddleBrown", "SaddleBrown")
+    canvas.draw_circle((313, 200), 5, 5, "black", "black")
+    canvas.draw_circle((280, 200), 5, 5, "black", "black")
+    canvas.draw_polygon([(320, 210), (285, 210), (312, 227)], 5, "Red", "Yellow")
+    canvas.draw_circle((200, 320), 50, 5, "Gold", "FireBrick")
+    canvas.draw_circle((220, 300), 50, 5, "FireBrick", "Gold")
+    canvas.draw_circle((300, 330), 90, 5, "SaddleBrown", "SaddleBrown")
    
     
-    # Right Eye
-    canvas.draw_line([90,100],[95,102], 5,"blue")
+  
+   # drawing the pumpkins 
+    canvas.draw_circle((400, 500), 60, 5, "Black", "DarkOrange")
+    canvas.draw_circle((525, 500), 60, 5, "Black", "DarkOrange")
+    canvas.draw_line((400, 500), (420, 590), 3, "Black")
+    
+    # Example: Drawing a point
+    canvas.draw_point((300, 200), "Blue")
 
 
-    
-    # Smile
-    canvas.draw_line([70, 130],[72, 132],5,"blue")
-    canvas.draw_line([74, 134],[76, 135], 5,"blue")
-    canvas.draw_line([78, 136],[80, 137], 5,"blue")
-    canvas.draw_line([82, 137],[84, 137], 5,"blue")
-    canvas.draw_line([86, 136],[88, 135], 5,"blue")
-    canvas.draw_line([90, 134],[92, 132], 5,"blue")
-    
-    
-# Create frame
-frame = simplegui.create_frame("CFU14 Happy Lines", 200, 200)
-frame.set_canvas_background("pink")
+    # Assign draw handler to the frame
 frame.set_draw_handler(draw_handler)
+#add button code
+frame.add_button("Click here!", click_button) #calls function
 
-# Start frame
+
+
+#start frame
 frame.start()
