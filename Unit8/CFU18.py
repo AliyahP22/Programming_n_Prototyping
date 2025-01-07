69a3c9576c74866acfed318df842b36426008912
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-01-07 09:29:47 -0500

    Update CFU18.py

--- Unit8/CFU18.py
+++ Unit8/CFU18.py
@@ -1,1 +1,9 @@
+#Aliyah Pargan 
+#Period 2
+#1-7-2025
+#CFU 18, which is a program that adds up all the prices in the following list
 
+prices = [1.95,4.50,0.99,5.99]
+
+for i in range(len(prices)):
+    
