e3a84336701c005b560d42cc107d37c8487052b0
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-02-27 22:42:31 -0500

    Update python_challenge2.py

--- Python_Challenges/python_challenge2.py
+++ Python_Challenges/python_challenge2.py
@@ -1,1 +1,13 @@
+#Aliyah Pargan
+#Periods 1-2
+#02-27-2025
+#Python Coding challenge 2, which tests our python skills using functions and len.
 
+
+def right_justify(s): #define function and parameters
+    leading_space = 70 - len(s) #calculates the amount of spaces 
+    print(' ' *leading_space +s) #' ' gives space and prints the string with leading space
+    
+    
+    
+right_justify("allen") #call function
