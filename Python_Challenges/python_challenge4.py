9202245152c29b3a5c51479dec6643bc7ed73eaf
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-03-05 20:14:33 -0500

    Update python_challenge4.py

--- Python_Challenges/python_challenge4.py
+++ Python_Challenges/python_challenge4.py
@@ -1,1 +1,20 @@
+#Aliyah Pargan
+#Periods 1-2
+#03-5-2025
+#Python Coding Challenge #4, which tests our ability to create and define functions that could
+#result in an image, like a grid
 
+
+def drawGrid():
+    for row in range(2):  # 4 rows
+        print("+", "-", "-", "-", "-", "+", "-", "-", "-", "+", sep='', end=' ')
+        print() 
+        for space in range(4):  
+            print("|", " " * 1, "|", " " * 1, "|", " " * 1)
+            print()  
+
+    
+    print("+", "-", "-", "-", "-", "+", "-", "-", "-", "+", sep='', end=' ')
+    print()  
+        
+drawGrid()
