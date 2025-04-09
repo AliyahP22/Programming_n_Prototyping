bda1d3820c709dbbc72c272d452a927e9d0285af
A.Pargan <144722355+AliyahP22@users.noreply.github.com>
2025-04-08 22:44:40 -0400

    Update pythonchallenge20.py

--- Python_Challenges/pythonchallenge20.py
+++ Python_Challenges/pythonchallenge20.py
@@ -1,1 +1,28 @@
+#Aliyah Pargan
+#Period 1
+#04-8-2025
+#Python Coding challenge 20
 
+def avoids(word, forbiddenLetters): #define function with both parameters
+    for letter in forbiddenLetters: #runs loop for each word in list
+        if letter in word:
+            return True
+        return False #returns false if condition executed is false
+    
+    
+def main_part(): #main function 
+    forbiddenLetters = input("Enter a string of forbidden letters!") #user inputs a letter
+    forbiddenLetters = input("Enter a string of forbidden letters!")
+    forbiddenLetters = input("Enter a string of forbidden letters!")
+    forbiddenLetters = input("Enter a string of forbidden letters!")
+    forbiddenLetters = input("Enter a string of forbidden letters!")
+    words_list = ["colors", "dog", "cat", "bird", "apple", "dress", "love", "grapes", "pretty",
+                  "hamster", "coding", "python", "computer", "radio" "banana", "papaya"]
+    count = 0 #initializer
+    for word in words_list:
+        if avoids(word, forbiddenLetters):
+            count+=1 #increases count
+    print(f"Total words without forbidden letters: {count}")
+            
+
+main_part() #call function   
